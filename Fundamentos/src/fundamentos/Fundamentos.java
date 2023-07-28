c4e1c2621a9a9a4a473236bb113bcea49fc56aa8
Henzo Felippe Fernandes de Oliveira <henzo.fernandes@aluno.senai.br>
2023-07-28 10:59:01 -0300

    Declarando String como objeto

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -14,11 +14,12 @@ public class Fundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    //Declaração de variavel
+    //Declaração de variaveis de tipos primitivos
     String nome = "Moisés";
     int idade = 40;
     boolean estudando = true;
     
+    String bairro = new String("Centro");
     }
     
 }
