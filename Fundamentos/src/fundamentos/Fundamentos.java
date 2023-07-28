b04d60ec49b108eaa8400a1ba06bee9388a182df
Henzo Felippe Fernandes de Oliveira <henzo.fernandes@aluno.senai.br>
2023-07-28 10:49:22 -0300

    Criando variaveis em Java

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -14,7 +14,11 @@ public class Fundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    //Declaração de variavel
+    String nome = "Moisés";
+    int idade = 40;
+    boolean estudando = true;
+    
     }
     
 }
