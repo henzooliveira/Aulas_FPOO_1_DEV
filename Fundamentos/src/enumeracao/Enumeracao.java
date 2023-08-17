3bb9c29b6711bb641a52096b33158054b5323925
henzo <henzo.fernandes@aluno.senai.br>
2023-08-17 10:12:58 -0300

    Aprendendo enumerações em java :)

---
+++ Fundamentos/src/enumeracao/Enumeracao.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Enumeracao {
+    public static void main(String[] args) {
+        Usuario usuario =new Usuario("Usuario01", "usuario@aluno.senai.br", true, TipoUsuarioEnum.TECNICO);
+        
+        System.out.println(usuario);
+        
+        Pedido pedido = new Pedido("Pedido do usuario01", 29.29, StatusEnum.EM_PREPARACAO);
+        System.out.println(pedido);
+    }
+}
---
+++ Fundamentos/src/enumeracao/TipoUsuarioEnum.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao;
+
+import javax.naming.CompoundName;
+
+/**
+ *
+ * @author Aluno
+ */
+public enum TipoUsuarioEnum {
+    COMUM, TECNICO, ADMNISTRADOR
+}
